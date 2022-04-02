fb7a434767adc66850d426d54ae5a77057eb5014
Kaili <kailicen226@gmail.com>
2022-04-03 08:29:04 +1000

    font

--- wiseup/static/javascript/nav_active.js
+++ wiseup/static/javascript/nav_active.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
     var currentLoc = window.location.href;
+
+    if (currentLoc.includes("?search=") === true) {
+        currentLoc = currentLoc.substring(0, currentLoc.indexOf("?"));
+    }
+
     $('li.nav-item a').each(function () {
         if (this.href === currentLoc) {
             $(this).addClass("active");
